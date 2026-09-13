cf861af69c5824dd99015d6a80766b2737add9e5
Anjali0708-gt <anj13062004@gmail.com>
2026-09-13 21:37:36 +0530

    Prepare backend for deployment

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import payementRoute from './routes/paymentRoute.js'
 // import serviceRoute from './routes/ServiceRoute.js'
 import measurementRoute from './routes/MeasurementRoute.js'
 import CartRoute from './routes/CartRoute.js'
-import teamMemberRoutes from "./routes/teamrote.js";
+// import teamMemberRoutes from "./routes/teamrote.js";
 import serviceRoutes from "./routes/ServiceRoute.js";
 import appointmentRoutes from "./routes/appointmentRoutes.js";
 
@@ -40,7 +40,7 @@ app.use("/api/cart", CartRoute);
 app.use("/api/payment", payementRoute);
 app.use("/api/Customer",Customerrouter);
 // app.use("/api/service",serviceRoute)
-app.use("/api/team", teamMemberRoutes);
+// app.use("/api/team", teamMemberRoutes);
 
 app.use("/api/report",reportRoute)
 app.use("/api/contact", ContactRoute);
